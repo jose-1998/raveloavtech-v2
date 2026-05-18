ca7b1fc6d9a753270017e4d7dd86521fe3d7516a
jose-1998 <josemrojasortega@gmail.com>
2026-05-17 19:40:56 -0500

    Expose Resend error detail for debugging

--- netlify/functions/send-quote.js
+++ netlify/functions/send-quote.js
@@ -159,7 +159,7 @@ exports.handler = async function (event) {
     return {
       statusCode: 500,
       headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: 'Failed to send email' }),
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
